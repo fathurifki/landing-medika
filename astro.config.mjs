ac11539db5535b01ebe6246e035c32c34a09f5a9
fathurifki <rifkifathu@gmail.com>
2024-06-25 16:58:44 +0700

    fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
         include: ['**/react/*']
     })],
     compressHTML: true,
-    output: 'server',
+    output: 'hybrid',
     security: {
         checkOrigin: true
     },
