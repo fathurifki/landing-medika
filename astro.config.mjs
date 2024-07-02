2425b13167b2f44deeafaf4d7f6499cd5993a0c1
fathurifki <rifkifathu@gmail.com>
2024-07-02 20:54:36 +0700

    release robot txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,20 +7,43 @@ import sitemap from '@astrojs/sitemap';
 import { loadEnv } from "vite";
 import robotsTxt from "astro-robots-txt";
 
+import fs from 'fs';
+import path from 'path';
+
+
 const {
-  VITE_SITE_URL
+    VITE_SITE_URL
 } = loadEnv(process.env.NODE_ENV || "", process.cwd(), "");
 
 
 // https://astro.build/config
 export default defineConfig({
-  site: VITE_SITE_URL,
-  integrations: [tailwind(), icons(), sitemap(), react({
-    include: ['**/react/*']
-  }), robotsTxt()],
-  compressHTML: true,
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+    site: VITE_SITE_URL,
+    integrations: [tailwind(), icons(), sitemap(), react({
+        include: ['**/react/*']
+    }), robotsTxt()],
+    compressHTML: true,
+    output: 'server',
+    adapter: node({
+        mode: "standalone"
+    }),
+    vite: {
+        build: {
+            rollupOptions: {
+                plugins: [
+                    {
+                        name: 'generate-robots-txt',
+                        writeBundle() {
+                            const robotsTxtContent = `
+                            User-agent: *
+                            Allow: /
+                            Sitemap: ${new URL('sitemap-index.xml', VITE_SITE_URL).href}`.trim();
+                            const outputPath = path.resolve(process.cwd(), 'dist/server/robots.txt');
+                            fs.writeFileSync(outputPath, robotsTxtContent);
+                        }
+                    }
+                ]
+            }
+        }
+    }
 });
